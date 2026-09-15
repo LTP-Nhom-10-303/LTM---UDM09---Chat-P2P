1892ed06d7af9f086764bee1198cbdaba8c12e10
namnd4775-hue <namnd4775@ut.edu.vn>
2026-09-15 12:50:20 +0700

    Update print statement from 'Hello' to 'Goodbye'

--- Code/GUI/chat_gui.py
+++ Code/GUI/chat_gui.py
@@ -12,10 +12,10 @@ from typing import Dict, List, Optional
 
 from PIL import Image, ImageTk
 
-from Gui.forward import build_forward_message
-from Gui.peer_list import PeerList
-from Gui.peer_manager import PeerManager
-from Gui.reply_emoji import EMOJIS, reply_preview
+from GUI.forward import build_forward_message
+from GUI.peer_list import PeerList
+from GUI.peer_manager import PeerManager
+from GUI.reply_emoji import EMOJIS, reply_preview
 from network.p2p_connection import P2PConnection
 from protocol.message_protocol import MessageProtocol
 
